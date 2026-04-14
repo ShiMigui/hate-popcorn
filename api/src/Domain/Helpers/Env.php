2481858c286df1b8a6506f109ed62becec62eb14
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-13 21:52:32 -0300

    feat: add static function isDevEnv

--- api/src/Domain/Helpers/Env.php
+++ api/src/Domain/Helpers/Env.php
@@ -8,8 +8,13 @@ use Hatepopcorn\Domain\Exceptions\EnvException;
 
 final class Env
 {
-    public static function get(string $name)
+    public static function get(string $name): mixed
     {
         return $_ENV[$name] ?? throw new EnvException($name);
+    }
+
+    public static function isDevEnv(): bool
+    {
+        return 'dev' === strtolower($_ENV['APP_ENV'] ?? '');
     }
 }
