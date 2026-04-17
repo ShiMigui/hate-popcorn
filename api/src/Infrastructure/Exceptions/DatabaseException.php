9ef169aed40ce941fe3e10bded84b26ab13ad56d
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-16 23:55:56 -0300

    refactor(infrastructure/exceptions): new parameter order for exception

--- api/src/Infrastructure/Exceptions/DatabaseException.php
+++ api/src/Infrastructure/Exceptions/DatabaseException.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Hatepopcorn\Infrastructure\Exceptions;
 
+use Hatepopcorn\Infrastructure\Database\PdoExceptionCodes;
+
 final class DatabaseException extends InfrastructureException
 {
-    public function __construct(\Throwable $e, int $appCode = 1000, string $message = 'Database Exception')
+    public readonly ?PdoExceptionCodes $type;
+    public readonly mixed $rawCode;
+
+    public function __construct(\Throwable $e, string $message = 'Database Exception', int $appCode = 0)
     {
+        $this->rawCode = $e->getCode();
+        $this->type    = PdoExceptionCodes::tryFrom($this->rawCode);
         parent::__construct($message, $appCode, $e);
     }
 }
